8e639e569957604d98f55e046a5843170e3a561b
Kipchumba Victor <victorkipchumba039@gmail.com>
2024-08-01 17:04:02 +0300

    Documentation correction

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,4 +5,7 @@ A module of add which sums 2 floats
 
 
 def add(a: float, b: float) -> float:
+    '''A function that sums two floats
+        Returns: sum of the floats
+    '''
     return float(a + b)
