3ac0595d287376d786e2a507e0ab5163bf5cf69e
Kipchumba Victor <victorkipchumba039@gmail.com>
2024-08-01 17:58:48 +0300

    Task 3: Basic annotation - to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''A module for to_str:
+    converts a float to a string
+'''
+
+
+def to_str(n: float) -> str:
+    '''A function to_str:
+        Returns: string representation of n
+    '''
+    return str(n)
