722ceb8983af8372fca481235fef12198e55e6d0
Kipchumba Victor <victorkipchumba039@gmail.com>
2024-08-06 08:58:46 +0300

    Task 0: Basic async syntax

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''wait_random module:
+    Delays for a random time
+'''
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,3 @@
+0x01-python_async_function
+----
+asynchronious python code
