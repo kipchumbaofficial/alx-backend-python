f62285605dc454fe7486a3a4387411724c3350b3
Kipchumba Victor <victorkipchumba039@gmail.com>
2024-08-06 10:42:41 +0300

    Task 3: Tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''task_wait_random module:
+    Creates a task
+'''
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''task_wait_random
+        Returns a task
+    '''
+    return asyncio.create_task(wait_random(max_delay))
