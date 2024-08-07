d3f8b71a30c99b357bddd327407bd45a3bad9128
Kipchumba Victor <victorkipchumba039@gmail.com>
2024-08-07 06:00:04 +0300

    Task 4

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''measure_runtime module:
+    measures run time
+'''
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    '''measure runtime function:
+        returns runtime
+    '''
+    tasks = [async_comprehension() for _ in range(4)]
+    start_time = time.time()
+    await asyncio.gather(*tasks)
+    end_time = time.time()
+
+    elapsed = (endtime - start_time) / 4
+    return elapsed
