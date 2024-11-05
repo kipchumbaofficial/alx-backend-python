e95dba493b16cf63cc925b85235a00d9f48a6707
victor <victorkipchumba039@gmail.com>
2024-11-05 04:32:19 +0300

    Task 1: test exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,3 +18,15 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: Any):
         """ Test the return value of access_nested_map """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")
+        ])
+    def test_access_nested_map_exception(
+            self, nested_map: Mapping,
+            path: Sequence, expected: Any):
+        """ Test the exceptions raised by access_nested_map """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{expected}'")
