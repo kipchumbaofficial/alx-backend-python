ab34b8648454ac2f5051891bc097c5079f879581
victor <victorkipchumba039@gmail.com>
2024-11-05 05:46:29 +0300

    Task 3: parametize an patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from typing import Mapping, Sequence, Any, Dict
 
 
@@ -55,3 +55,40 @@ class TestGetJson(unittest.TestCase):
 
         # Assert that the response from get_json matches the expected
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Test memoize
+    """
+    def test_memoize(self):
+        """ tests if memoize works
+        """
+        class TestClass:
+            """ Test class with a memoized property
+            """
+            def a_method(self):
+                '''Returns 42'''
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        # Use patch to mock a_method
+        with patch.object(
+                TestClass,
+                'a_method',
+                return_value=42) as mock_method:
+            # Instantiate the class
+            test_instance = TestClass()
+
+            # Access the memoized property twice
+            result_first_call = test_instance.a_property
+            result_second_call = test_instance.a_property
+
+            # Assert that the results are as expected
+            self.assertEqual(result_first_call, 42)
+            self.assertEqual(result_second_call, 42)
+
+            # Assert a_method was only called once
+            mock_method.assert_called_once()
